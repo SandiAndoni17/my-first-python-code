09a5cf220f9ace51a622f378cec5f623a8409db7
Sandianoni17 <sandiandoni17@gmail.com>
2024-02-10 17:22:13 +0700

    susu telur

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 """
-Ini adalah demo projek pertama dengan python
+This is my first project in Python
+Today i am gonna tell about Sequencial
 
 """
-print('helo word')
-print('my name is sandi')
-print('i am member of rwid')
+print('Mom said, "Buy a bottle of milk, If any eggs buy also 6!".')
+print('OK MOM')
+print('Beli Susu')
+
+milk = input('any telur?[y/n]:')
+if milk == 'y':
+    print('I buy 6 eggs and bring to mom')
+else:
+    print('pulang and bring milk to mom')
